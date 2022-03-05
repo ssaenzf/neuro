e90a858ff24c4fa8425b147be97f77b1873fd900
ZhijieQ <zhijie.qian@estudiante.uam.es>
2022-03-05 20:24:54 +0100

    ..

--- pract1/simulaciones.py
+++ pract1/simulaciones.py
@@ -8,15 +8,15 @@ import os
 # por que no llevara a ninguna solución óptima y sería computacionalmente costoso
 
 umbral = 0.1
-while umbral <= 1.5:
-    umbral = umbral * 2
-    epocas = 10
+while umbral <= 1:
+    umbral += 0.1
+    epocas = 100
     while epocas <= 1000:
-        epocas = epocas * 2
-        alpha = 0.001
-        while alpha <= 0.33:
+        epocas += 100
+        alpha = 0.01
+        while alpha <= 1:
             alpha = alpha * 3
             print(f"Perceptron, umbral {umbral}, epocas {epocas}, alpha {alpha}")
-            os.system(f'python perceptron.py --modo3 data/problema_real2.txt data/problema_real2_no_etiquetados.txt --f_out output/parameter_tuning/problema2/perceptron/output_umbral{umbral}_epocas{epocas}_alpha{alpha}.txt --umbral {umbral} --alpha {alpha} --epoca {epocas}')
+            os.system(f'python perceptron.py --modo1 data/problema_real1.txt 0.25 --f_out output/parameter_tuning/problema1/perceptron/output_umbral{umbral}_epocas{epocas}_alpha{alpha}.txt --umbral {umbral} --alpha {alpha} --epoca {epocas}')
             print(f"Adaline, umbral {umbral}, epocas {epocas}, alpha {alpha}")
-            os.system(f'python Adaline.py --modo3 data/problema_real2.txt data/problema_real2_no_etiquetados.txt --f_out output/parameter_tuning/problema2/adaline/output_umbral{umbral}_epocas{epocas}_alpha{alpha}.txt --umbral {umbral} --alpha {alpha} --torelancia 0.01 --epoca {epocas}')
+            os.system(f'python adaline.py --modo1 data/problema_real1.txt 0.25 --f_out output/parameter_tuning/problema1/adaline/output_umbral{umbral}_epocas{epocas}_alpha{alpha}.txt --umbral {umbral} --alpha {alpha} --torelancia 0.01 --epoca {epocas}')
