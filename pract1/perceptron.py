8d537517f744a9eb197a3ed7b93d9529328bd28f
ZhijieQ <zhijie.qian@estudiante.uam.es>
2022-03-05 20:20:07 +0100

    ..

--- pract1/perceptron.py
+++ pract1/perceptron.py
@@ -284,8 +284,9 @@ if __name__ == '__main__':
         X_train, X_test, y_train, y_test = LeerFichero.mode1(args.modo1[0], args.modo1[1])
         perceptron = Perceptron(umbral=umbral, alpha=alpha, epoca=epoca)
         perceptron.train(X_train, y_train)
-        # perceptron.score(X_train, y_train)
+        
         perceptron.test(X_test, y_test, f_out)
+        perceptron.score(X_test, y_test)
 
     elif args.modo2:
         X, y = LeerFichero.mode2(args.modo2[0])
@@ -297,7 +298,7 @@ if __name__ == '__main__':
         X_train, X_test, y_train, y_test = LeerFichero.mode3(args.modo3[0], args.modo3[1])
         perceptron = Perceptron(umbral=umbral, alpha=alpha, epoca=epoca)
         perceptron.train(X_train, y_train)
-        perceptron.score(X_train, y_train)
+        # perceptron.score(X_train, y_train)
         perceptron.test(X_test, y_test, f_out)
     else:
         print("Error en los argumentos, necesita especificar algun modo de operacion.")
