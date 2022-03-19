db68b7831f636b60b8c638f35e55584143c67b59
Santos Saenz <98255286+ssaenzf@users.noreply.github.com>
2022-03-19 11:33:58 +0100

    script_creado, en ejecuccion

--- pract2/hyperparameter_tuning.py
+++ pract2/hyperparameter_tuning.py
@@ -1,9 +1,7 @@
 from multicapa import Multicapa
 from leerFichero import LeerFichero
 import csv
-
-# TODO: https://www.pythontutorial.net/python-basics/python-write-csv-file/
-header = ['problema', 'alpha', 'capa_oculta', 'num_epocas', 'score', 'matriz_confusion']
+import pandas as pd
 
 # problema_real 1, 2, 3, 4, 5, 6 Sin normalizar
 ficheros_entrada = ["data/problema_real1.txt",
@@ -19,9 +17,10 @@ epocas = [1500, 1500, 1500]
 norm = False
 test_size = 0.25
 fichero_scores = "data/hyperparameter_tuning_sin_norm.csv"
-f = open(fichero_scores, "w")
+df_cols = ['problema', 'alpha' , 'capa_oculta', 'num_epocas', 'score']
+#with column names
+df = pd.DataFrame(columns=df_cols)
 for fichero in ficheros_entrada:
-    f.writelines([fichero])
     for epoca in epocas:
         for alpha in coeficientes_alpha:
             # Se selecciona el número de capas
@@ -33,11 +32,13 @@ for fichero in ficheros_entrada:
                 score = red.score(X_test, y_test)
                 num_epocas = lista_epoca[-1]
                 matriz = red.matriz_confusion(y_test, y_preds)
-                # TODO: csv con formato: problema, alpha, capa_oculta, num_epocas, score, matriz_confusion
+                # TODO: csv con formato: problema, alpha, capa_oculta, num_epocas, score
+                new_row = {'problema':fichero[5:], 'alpha':alpha, 'configuracion_capas_neurona': neuronas, 'score': score}
+                #append row to the dataframe
+                df = df.append(new_row, ignore_index=True)
                 print(f'Ejecutado: {fichero}, {alpha}, {neuronas}, {num_epocas}, {score}')
-                # f.writelines([f"Validacion con red neuronal con 1 capa oculta con {neuronas} neuronas, alpha {alpha}, epocas {epoca}", 
-                #               f"Porcentaje de aciertos: {red.score(X_test, y_test)}%\n"])
-f.close()
+    # Cuando se haya procesado un problema se sobreescrbira el csv
+    df.to_csv(fichero_scores)
 
 # problema_real 4, 6 Con normalizar
 ficheros_entrada = ["data/problema_real4.txt",
@@ -49,9 +50,8 @@ epocas = [1500, 1500, 1500]
 norm = True
 test_size = 0.25
 fichero_scores = "data/hyperparameter_tuning_con_norm.csv"
-f = open(fichero_scores, "w")
+df = pd.DataFrame(columns=df_cols)
 for fichero in ficheros_entrada:
-    f.writelines([fichero])
     for epoca in epocas:
         for alpha in coeficientes_alpha:
             # Se selecciona el número de capas
@@ -63,11 +63,15 @@ for fichero in ficheros_entrada:
                 score = red.score(X_test, y_test)
                 num_epocas = lista_epoca[-1]
                 matriz = red.matriz_confusion(y_test, y_preds)
+                new_row = {'problema':fichero[5:], 'alpha':alpha, 'configuracion_capas_neurona': neuronas, 'score': score}
+                #append row to the dataframe
+                df = df.append(new_row, ignore_index=True)
                 # TODO: csv con formato: problema, alpha, capa_oculta, num_epocas, score, matriz_confusion
                 print(f'Ejecutado: {fichero}, {alpha}, {neuronas}, {num_epocas}, {score}')
                 # f.writelines([f"Validacion con red neuronal con 1 capa oculta con {neuronas} neuronas, alpha {alpha}, epocas {epoca}", 
                 #               f"Porcentaje de aciertos: {red.score(X_test, y_test)}%\n"])
-f.close()
+    # Cuando se haya procesado un problema se sobreescrbira el csv
+    df.to_csv(fichero_scores)
 
 
 # problema_real 2
