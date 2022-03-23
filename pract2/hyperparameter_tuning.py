16092275002bf932c5b61abc18585cf6cf1a972a
Santos Saenz <98255286+ssaenzf@users.noreply.github.com>
2022-03-23 16:19:21 +0100

    Procesamiento ej2 sin norm, norm a medias

--- pract2/hyperparameter_tuning.py
+++ pract2/hyperparameter_tuning.py
@@ -4,12 +4,16 @@ import csv
 import pandas as pd
 
 # problema_real 1, 2, 3, 4, 5, 6 Sin normalizar
+"""
 ficheros_entrada = ["data/problema_real1.txt",
                     "data/problema_real2.txt",
                     "data/problema_real3.txt",
                     "data/problema_real4.txt",
                     "data/problema_real5.txt",
                     "data/problema_real6.txt"]
+"""
+"""
+ficheros_entrada = ["data/problema_real6.txt"]
 coeficientes_alpha = [0.001, 0.003, 0.01, 0.03, 0.01, 0.03, 0.1, 0.3]
 numero_neuronas_capa = [[], [2], [4], [8], [2, 2], [4, 2], [4, 4]]
 tolerancia = 0.005
@@ -19,7 +23,8 @@ test_size = 0.25
 fichero_scores = "data/hyperparameter_tuning_sin_norm.csv"
 df_cols = ['problema', 'alpha' , 'capa_oculta', 'num_epocas', 'score']
 #with column names
-df = pd.DataFrame(columns=df_cols)
+df = pd.read_csv(fichero_scores)
+df = df.drop('Unnamed: 0',axis=1)
 for fichero in ficheros_entrada:
     for epoca in epocas:
         for alpha in coeficientes_alpha:
@@ -33,24 +38,27 @@ for fichero in ficheros_entrada:
                 num_epocas = lista_epoca[-1]
                 matriz = red.matriz_confusion(y_test, y_preds)
                 # TODO: csv con formato: problema, alpha, capa_oculta, num_epocas, score
-                new_row = {'problema':fichero[5:], 'alpha':alpha, 'configuracion_capas_neurona': neuronas, 'score': score}
+                new_row = {'problema':fichero[5:], 'alpha':alpha, 'capa_oculta': neuronas, 'num_epocas': epoca,'score': score}
                 #append row to the dataframe
                 df = df.append(new_row, ignore_index=True)
                 print(f'Ejecutado: {fichero}, {alpha}, {neuronas}, {num_epocas}, {score}')
     # Cuando se haya procesado un problema se sobreescrbira el csv
     df.to_csv(fichero_scores)
-
+"""
 # problema_real 4, 6 Con normalizar
-ficheros_entrada = ["data/problema_real4.txt",
-                    "data/problema_real6.txt"]
-numero_neuronas_capa = [[], [2], [4], [8], [2, 2], [4, 2], [4, 4]]
+#ficheros_entrada = ["data/problema_real4.txt", "data/problema_real6.txt"]
+ficheros_entrada = ["data/problema_real6.txt"]
+df_cols = ['problema', 'alpha' , 'capa_oculta', 'num_epocas', 'score']
+numero_neuronas_capa = [[], [2], [4], [8]]
 tolerancia = 0.005
 coeficientes_alpha = [0.001, 0.003, 0.01, 0.03, 0.01, 0.03, 0.1, 0.3]
 epocas = [1500, 1500, 1500]
 norm = True
 test_size = 0.25
 fichero_scores = "data/hyperparameter_tuning_con_norm.csv"
-df = pd.DataFrame(columns=df_cols)
+#df = pd.DataFrame(columns=df_cols)
+df = pd.read_csv(fichero_scores)
+df = df.drop('Unnamed: 0',axis=1)
 for fichero in ficheros_entrada:
     for epoca in epocas:
         for alpha in coeficientes_alpha:
@@ -63,7 +71,7 @@ for fichero in ficheros_entrada:
                 score = red.score(X_test, y_test)
                 num_epocas = lista_epoca[-1]
                 matriz = red.matriz_confusion(y_test, y_preds)
-                new_row = {'problema':fichero[5:], 'alpha':alpha, 'configuracion_capas_neurona': neuronas, 'score': score}
+                new_row = {'problema':fichero[5:], 'alpha':alpha, 'capa_oculta': neuronas, 'num_epocas': epoca,'score': score}
                 #append row to the dataframe
                 df = df.append(new_row, ignore_index=True)
                 # TODO: csv con formato: problema, alpha, capa_oculta, num_epocas, score, matriz_confusion
